45b16e92470ece6abf0763ff9536c9c13949b9b8
Diogo Cezar Teixeira Batista <diogo.batista@meifacil.com>
2019-11-24 17:30:14 -0300

    start

--- src/controllers/BanksController.js
+++ src/controllers/BanksController.js
@@ -2,38 +2,6 @@ const { Bank, Rating } = require('../models')
 
 class BanksController {
   async show(req, res) {
-    const { id_user: idUser } = req.headers
-    try {
-      const banks = await Bank.findAll()
-      const promises = banks.map(async item => ({
-        id: item.id,
-        name: item.name,
-        icon: item.icon,
-        code: item.code,
-        generalRating: await BanksController.extractRating(item.id),
-        myRating: await BanksController.extractMyRating(idUser, item.id),
-      }
-      ))
-      const jsonReturn = await Promise.all(promises)
-      return res.status(200).json(jsonReturn)
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({ error: true })
-    }
-  }
-
-  static async extractRating(idBank) {
-    const databaseRating = await Rating.findAll({ where: { idBank } })
-    if (!databaseRating.length) return 0
-    const { rating: sumRatings } = databaseRating.reduce((oldValue, newValue) => ({ rating: oldValue.rating + newValue.rating }))
-    const avgRatings = Math.round(sumRatings / databaseRating.length)
-    return avgRatings
-  }
-
-  static async extractMyRating(idUser, idBank) {
-    const [databaseRating] = await Rating.findAll({ where: { idUser, idBank }, order: [['created_at', 'DESC']] })
-    if (!databaseRating) return 0
-    return databaseRating.rating
   }
 }
 
--- src/controllers/RatingsController.js
+++ src/controllers/RatingsController.js
@@ -2,20 +2,6 @@ const { Rating } = require('../models')
 
 class RatingsController {
   async create(req, res) {
-    try {
-      const { idUser, idBank, rating } = req.body
-      if (!idUser || !idBank || !rating) { throw new Error('Missing entry properties.') }
-      const result = await Rating.create({
-        idUser,
-        idBank,
-        rating,
-      })
-      if (result) { return res.status(200).json({ success: true }) }
-      throw new Error('Error trying to create rating.')
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({ error: true })
-    }
   }
 }
 
